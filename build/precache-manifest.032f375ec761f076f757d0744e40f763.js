9a1463dea6a37710f435a74076d55bfad9f16c5a
rookjeknaja1 <dsitsy@test1.asia-southeast1-b.c.centered-moon-282508.internal>
2020-08-18 09:09:13 +0000

    test

---
+++ build/precache-manifest.032f375ec761f076f757d0744e40f763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb422f719bae0c999ef9a5da54a55636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77426ddac9f298052176",
+    "url": "/static/css/main.8ddc32fd.chunk.css"
+  },
+  {
+    "revision": "a3961e171a33809b2661",
+    "url": "/static/js/2.57848404.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57848404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77426ddac9f298052176",
+    "url": "/static/js/main.c8c16aca.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
